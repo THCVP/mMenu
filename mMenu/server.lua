a40082220fde478d43350b34abb8569cb28076a2
THC <88347509+THCVP@users.noreply.github.com>
2021-11-13 04:03:34 -0800

    Delete mMenu directory

--- mMenu/server.lua
+++
@@ -1,6 +0,0 @@
-
-local resname = GetCurrentResourceName()
-print("\n--------------------------------------------------------------------------")
-print("\n"..resname.." ^1has started")
-print("\n^4Author: ^2Mosheba")
-print("\n--------------------------------------------------------------------------")
